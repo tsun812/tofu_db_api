fd135a03fd44b6939358489214c99518a8635a07
samsam3268 <samskleung@gmail.com>
2022-02-11 18:32:18 -0800

    fix CORS and sqlite

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# Allow other application to access the API
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
