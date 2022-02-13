5e63dbc9977aacb082fada0200202134047432af
samsam3268 <samskleung@gmail.com>
2022-02-12 21:27:57 -0800

    fixed records and record model

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,9 +1,18 @@
 class RecordsController < ApplicationController
+  def createValue(record_id, field_id)
+    Value.create(field_value: nil, record_id: record_id, field_id: field_id)
+  end
+
   def create
     records = Record.create(
         position: params[:position],
         application_id: params[:application_id]
     )
+
+    allfields = Field.where(application_id: params[:application_id] )
+    allfields.each do |field|
+      createValue(records.id, field.id)
+    end
     render json: records
   end
 
@@ -22,5 +31,7 @@ end
     )
     render json: record
   end
+
 end
+
 
