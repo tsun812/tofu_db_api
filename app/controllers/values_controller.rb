e01f07c39fab10e44b1fc09d338116970c40d6b5
Yan Sun <tsun812@gmail.com>
2022-02-12 17:54:32 -0800

    add helper function in values controller

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,2 +1,14 @@
 class ValuesController < ApplicationController
+  def create(record_id, field_id)
+    Record.create(record_value: params[:record_value], record_id: record_id, field_id: field_id)
+  end
+
+  def update(record_id, field_id)
+    Record.where(record_id: record_id, field_id: field_id).update(record_value:params[:record_value])
+  end
+
+  def destroy(record_id, field_id)
+    Record.where(record_id: record_id, field_id: field_id).destroy
+  end
+
 end
