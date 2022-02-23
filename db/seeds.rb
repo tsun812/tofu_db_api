f2fa18fc3560ed283d1e6890f2ce82ff386527b4
Yan Sun <tsun812@gmail.com>
2022-02-22 20:16:11 -0800

    change seed for presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ user1 = User.create(username: "Sam")
 user2 = User.create(username: "Yan")
 user3 = User.create(username: "Matt")
 
-application1 = user1.applications.create! ({primary_field: "Word", secondary_field: "Origin", background_color: "black", description: "This is a fruit dictionary app", font: 12, display_theme: "List", img_url: "https://images.unsplash.com/photo-1491841651911-c44c30c34548?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwYm9va3N8ZW58MHx8MHx8&w=1000&q=80", app_name: "Fruit Dictionary"})
+application1 = user1.applications.create! ({primary_field: "Word", secondary_field: "Color", background_color: "black", description: "This is a fruit dictionary app", font: 12, display_theme: "List", img_url: "https://images.unsplash.com/photo-1491841651911-c44c30c34548?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwYm9va3N8ZW58MHx8MHx8&w=1000&q=80", app_name: "Fruit Dictionary"})
 application2 = user1.applications.create! ({primary_field: "Building Name", secondary_field: "Price", background_color: "green", description: "This is a building app", font: 14, display_theme: "Card", img_url: "https://luxurylaunches.com/wp-content/uploads/2021/09/1000-year-old-castle-on-sale-6.jpg", app_name: "Real Estate Directory"})
 application3 = user2.applications.create! ({primary_field: "Event", secondary_field: "Bread", background_color: "pink,", description: "This is a food app", font: 20, display_theme: "Table", img_url: "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2F84f922d0-9bfe-11e8-9b62-17ec317258a6.jpg?crop=768%2C432%2C0%2C40", app_name: "Grandma's Recipe Book"})
 
